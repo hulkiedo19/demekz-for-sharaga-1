e1e75422039982faf4affed75d0c34461f7f7f0e
hulkiedo19 <artemmironovhaker@gmail.com>
2022-08-09 19:40:58 +0300

    worked version without text searching, sort and filter

--- demekz3/MainWindow.xaml.cs
+++ demekz3/MainWindow.xaml.cs
@@ -16,11 +16,13 @@ using System.Windows.Shapes;
 
 namespace demekz3
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        static List<Product> All_products = null;
+        static List<List<Product>> All_pages = null;
+        static int pages_count = 0;
+        static int current_page = 0;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -39,75 +41,174 @@ namespace demekz3
                 "Without materials"
             };
 
-            // okay, you can add buttons to stack panel through code
-            //PagesPanel.Children.Add(new Button() { Content = "123" });
+            // Getting list with all elements in database
+            All_products = GetProducts();
 
-            UpdateListView();
+            // getting number of full pages, that contains 4 elements
+            int pages = All_products.Count / 4;
+            // getting number of elements on last page
+            int last_page_elements = All_products.Count % 4;
+
+            // splitting all elements into pages
+            GetPages(pages, last_page_elements);
+
+            pages_count = All_pages.Count;
+            SetPage();
+
+            SetButtons();
         }
 
-        private void UpdateListView()
+        List<Product> GetProducts()
         {
-            using (var DbContext = new DatabaseEntities())
+            List<Product> products = null;
+            using(var DbContext = new DatabaseEntities())
             {
-                var items = DbContext.Products.ToList();
+                products = DbContext.Products.ToList();
+            }
+            return products;
+        }
 
-                // ----------------- SORT ITEMS -----------------
-                if (ComboSort.SelectedIndex >= 0)
+        void GetPages(int count, int last_page_elements)
+        {
+            All_pages = new List<List<Product>>();
+            int element_index = 0;
+
+            // here we sets all 4-element pages
+            for(int i = 0; i < count; i++)
+            {
+                List<Product> page = new List<Product>();
+
+                for(int j = 0; j < 4; j++)
                 {
-                    // TODO: материалы херово сортирует
-                    switch (ComboSort.SelectedIndex)
-                    {
-                        case 0:
-                            items = items.OrderBy(p => p.ProductCost).ToList();
-                            break;
-                        case 1:
-                            items = items.OrderByDescending(p => p.ProductCost).ToList();
-                            break;
-                        case 2:
-                            items = items.OrderByDescending(p => p.Materials).ToList();
-                            break;
-                        case 3:
-                            items = items.OrderBy(p => p.Materials).ToList();
-                            break;
-                    }
+                    page.Add(All_products[element_index++]);
                 }
 
-                // ----------------- FILTER ITEMS -----------------
+                All_pages.Add(page);
+            }
 
-                if(ComboFilter.SelectedIndex >= 0)
+            // if there are elements left, another page will then be added
+            if(last_page_elements > 0)
+            {
+                List<Product> page = new List<Product>();
+
+                for(int i = 0; i < last_page_elements; i++)
                 {
-                    switch(ComboFilter.SelectedIndex)
-                    {
-                        case 0:
-                            items = items.Where(p => p.Materials != null).ToList();
-                            break;
-                        case 1:
-                            items = items.Where(p => p.Materials == null).ToList();
-                            break;
-                    }
+                    page.Add(All_products[element_index++]);
                 }
 
-                // ----------------- TEXT SEARCH -----------------
-                if (TextBoxSearch.Text != "")
-                    items = items.Where(p => p.ProductName.ToLower().Contains(TextBoxSearch.Text.ToLower())).ToList();
-
-                ListView1.ItemsSource = items;
+                All_pages.Add(page);
             }
         }
 
+        void SetPage()
+        {
+            ListView1.ItemsSource = All_pages[current_page];
+        }
+
+        void SetButtons()
+        {
+            // sets page left button
+            Button left_page = new Button();
+            left_page.Content = "<";
+            left_page.Margin = new Thickness(0, 0, 2, 0);
+            left_page.BorderBrush = new SolidColorBrush(Colors.White);
+            left_page.BorderThickness = new Thickness(0, 0, 0, 0);
+            left_page.Background = new SolidColorBrush(Colors.White);
+            left_page.Click += PageLeft_Click;
+
+            PagesPanel.Children.Add(left_page);
+
+            // sets main numbered buttons
+            for(int i = 0; i < All_pages.Count; i++)
+            {
+                Button number_page = new Button();
+                number_page.Content = $"{i + 1}";
+                number_page.Margin = new Thickness(2, 0, 2, 0);
+                number_page.BorderBrush = new SolidColorBrush(Colors.White);
+                number_page.BorderThickness = new Thickness(0, 0, 0, 0);
+                number_page.Background = new SolidColorBrush(Colors.White);
+                number_page.Click += specified_page_button_click;
+
+                PagesPanel.Children.Add(number_page);
+            }
+
+            // sets page left button
+            Button right_page = new Button();
+            right_page.Content = ">";
+            right_page.Margin = new Thickness(2, 0, 0, 0);
+            right_page.BorderBrush = new SolidColorBrush(Colors.White);
+            right_page.BorderThickness = new Thickness(0, 0, 0, 0);
+            right_page.Background = new SolidColorBrush(Colors.White);
+            right_page.Click += PageRight_Click;
+
+            PagesPanel.Children.Add(right_page);
+        }
+
+        void specified_page_button_click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+
+            int page_number = Convert.ToInt32(button.Content);
+
+            current_page = page_number - 1;
+            SetPage();
+        }
+
+        private void PageLeft_Click(object sender, RoutedEventArgs e)
+        {
+            if (current_page == 0)
+                return;
+
+            current_page--;
+            SetPage();
+        }
+
+        private void PageRight_Click(object sender, RoutedEventArgs e)
+        {
+            if (current_page == (pages_count - 1))
+                return;
+
+            current_page++;
+            SetPage();
+        }
+
+        // these methods are doesn't work
         private void TextBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
-            UpdateListView();
+            //UpdateListView();
         }
 
         private void ComboSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            UpdateListView();
+            //UpdateListView();
         }
 
         private void ComboFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            UpdateListView();
+            //UpdateListView();
         }
+
+        /*private void UpdateListView() {
+            var items = DbContext.Products.ToList();
+            // ----------------- SORT ITEMS -----------------
+            if (ComboSort.SelectedIndex >= 0) {
+                // TODO: материалы херово сортирует
+                switch (ComboSort.SelectedIndex) {
+                    case 0: items = items.OrderBy(p => p.ProductCost).ToList(); break;
+                    case 1: items = items.OrderByDescending(p => p.ProductCost).ToList(); break;
+                    case 2: items = items.OrderByDescending(p => p.Materials).ToList(); break;
+                    case 3: items = items.OrderBy(p => p.Materials).ToList(); break;
+                }
+            }
+            // ----------------- FILTER ITEMS -----------------
+            if(ComboFilter.SelectedIndex >= 0) {
+                switch(ComboFilter.SelectedIndex) {
+                    case 0: items = items.Where(p => p.Materials != null).ToList(); break;
+                    case 1: items = items.Where(p => p.Materials == null).ToList(); break;
+                }
+            }
+            // ----------------- TEXT SEARCH -----------------
+            if (TextBoxSearch.Text != "") items = items.Where(p => p.ProductName.ToLower().Contains(TextBoxSearch.Text.ToLower())).ToList();
+            ListView1.ItemsSource = items; }*/
     }
 }
